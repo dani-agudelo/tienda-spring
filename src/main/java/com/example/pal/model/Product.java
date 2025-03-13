992d3df597067dad33377b3d1813e2706981efa8
dani-agudelo <daniiagudelom@gmail.com>
2025-03-13 07:43:28 -0500

    feat: Product Model created

---
+++ src/main/java/com/example/pal/model/Product.java
@@ -0,0 +1,30 @@
+package com.example.pal.model;
+
+import jakarta.persistence.*;
+import java.util.Set;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private Double price;
+
+//   @ManyToMany
+//   @JoinTable(
+//       name = "product_categories",
+//       joinColumns = @JoinColumn(name = "product_id"),
+//       inverseJoinColumns = @JoinColumn(name = "category_id"))
+//   private Set<Category> categories;
+}
