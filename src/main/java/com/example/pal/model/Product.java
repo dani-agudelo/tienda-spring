3f9711bd27317460c8f3cbc8e40765c0db336764
dani-agudelo <daniiagudelom@gmail.com>
2025-03-13 07:53:03 -0500

    feat: Product and Category repositories created

--- src/main/java/com/example/pal/model/Product.java
+++ src/main/java/com/example/pal/model/Product.java
@@ -8,11 +8,12 @@ import lombok.Data;
 @Entity
 @Table(name = "products")
 public class Product {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private String name;
 
   @Column(nullable = false)
